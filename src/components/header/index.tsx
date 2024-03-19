f5b0db6f6ef7da1d315c6fc37555bb561e1cfcb0
StefanyVasc <stefany.vasc.sa@gmail.com>
2024-03-19 00:15:24 -0300

    chore: adjust Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom'
 
 import { DateRange } from '../input/dateRange'
 import { Months } from '../months'
@@ -6,6 +7,21 @@ import * as S from './styles'
 
 export function Header() {
   const [title, setTitle] = useState('Summary')
+  const location = useLocation()
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setTitle('Summary')
+        document.title = 'Fintech | Summary'
+        break
+
+      case '/sales':
+        setTitle('Sales')
+        document.title = 'Fintech | Sales'
+        break
+    }
+  }, [location])
 
   return (
     <header className="mb">
